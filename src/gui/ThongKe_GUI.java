a114e41e28d4a457fbad171d8403ef2d66203052
datle0910 <va.09102003@gmail.com>
2024-11-02 22:10:22 +0700

    update thongke

--- src/gui/ThongKe_GUI.java
+++ src/gui/ThongKe_GUI.java
@@ -9,6 +9,9 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JButton;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import javax.swing.JRadioButton;
 
 public class ThongKe_GUI extends SimpleForm {
 
@@ -25,55 +28,83 @@ public class ThongKe_GUI extends SimpleForm {
 		add(pnCenter, BorderLayout.CENTER);
 		pnCenter.setLayout(null);
 		
-		JPanel panel =  new JPanel();
-		panel.setBounds(94, 114, 326, 246);
-		pnCenter.add(panel);
+		JLabel lblNewLabel = new JLabel("Thống Kê Và Báo Cáo");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 17));
+		lblNewLabel.setBounds(10, 10, 290, 44);
+		pnCenter.add(lblNewLabel);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(22, 390, 1468, 400);
-		pnCenter.add(panel_2);
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel.setBounds(10, 53, 1480, 61);
+		pnCenter.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnSanPham = new JButton("Sản Phẩm");
+		btnSanPham.setBounds(10, 10, 85, 37);
+		panel.add(btnSanPham);
+		
+		JButton btnNhanVien = new JButton("Nhân Viên");
+		btnNhanVien.setBounds(122, 10, 85, 37);
+		panel.add(btnNhanVien);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(594, 114, 326, 246);
+		panel_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_1.setBounds(10, 133, 852, 137);
 		pnCenter.add(panel_1);
+		panel_1.setLayout(null);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(10, 10, 1480, 94);
-		pnCenter.add(panel_4);
-		panel_4.setLayout(null);
+		JLabel lblNewLabel_1 = new JLabel("Thời Gian");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(10, 10, 196, 34);
+		panel_1.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel = new JLabel("Thống Kê");
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 18));
-		lblNewLabel.setBounds(30, 10, 187, 74);
-		panel_4.add(lblNewLabel);
+		JLabel lblNewLabel_2 = new JLabel("Ngày Bắt Đầu");
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(10, 72, 100, 34);
+		panel_1.add(lblNewLabel_2);
 		
 		JDateChooser dcNgayBatDau = new JDateChooser();
-		dcNgayBatDau.setToolTipText("Từ Ngày");
-		dcNgayBatDau.setBounds(423, 30, 253, 31);
-		panel_4.add(dcNgayBatDau);
+		dcNgayBatDau.setBounds(120, 72, 196, 34);
+		panel_1.add(dcNgayBatDau);
 		
-		JDateChooser dcNgayBatDau_1 = new JDateChooser();
-		dcNgayBatDau_1.setBounds(974, 30, 253, 31);
-		panel_4.add(dcNgayBatDau_1);
+		JLabel lblNewLabel_2_1 = new JLabel("Ngày Kết Thúc");
+		lblNewLabel_2_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_2_1.setBounds(386, 72, 100, 34);
+		panel_1.add(lblNewLabel_2_1);
 		
-		JButton btnXacNhan = new JButton("Xác Nhận");
-		btnXacNhan.setFont(new Font("Arial", Font.BOLD, 12));
-		btnXacNhan.setBounds(1278, 30, 93, 31);
-		panel_4.add(btnXacNhan);
+		JDateChooser dcNgayKetThuc = new JDateChooser();
+		dcNgayKetThuc.setBounds(505, 72, 196, 34);
+		panel_1.add(dcNgayKetThuc);
 		
-		JLabel lblNewLabel_1 = new JLabel("Từ Ngày");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblNewLabel_1.setBounds(333, 30, 87, 31);
-		panel_4.add(lblNewLabel_1);
+		JButton btnTruyVan = new JButton("Truy Vấn");
+		btnTruyVan.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnTruyVan.setBounds(735, 72, 105, 34);
+		panel_1.add(btnTruyVan);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Đến Ngày");
-		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblNewLabel_1_1.setBounds(877, 30, 87, 31);
-		panel_4.add(lblNewLabel_1_1);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 297, 1480, 493);
+		pnCenter.add(panel_2);
 		
-		JPanel panel_1_1 = new JPanel();
-		panel_1_1.setBounds(1095, 114, 326, 246);
-		pnCenter.add(panel_1_1);
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_3.setBounds(883, 133, 607, 137);
+		pnCenter.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("Sản Phẩm");
+		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(10, 10, 236, 34);
+		panel_3.add(lblNewLabel_3);
+		
+		JRadioButton rdo_spBanChay = new JRadioButton("Sản Phẩm Bán Chạy");
+		rdo_spBanChay.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdo_spBanChay.setBounds(71, 69, 198, 44);
+		panel_3.add(rdo_spBanChay);
+		
+		JRadioButton rdo_spBanCham = new JRadioButton("Sản Phẩm Bán Chậm");
+		rdo_spBanCham.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdo_spBanCham.setBounds(301, 69, 198, 44);
+		panel_3.add(rdo_spBanCham);
 
 	}
 }
